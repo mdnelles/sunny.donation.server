8cedae718632d6ad2f27dae3234b0c775fba91a7
mdnelles <mxnelles@gmail.com>
2020-05-19 21:03:02 -0400

    update

--- routes/MediaRoutes.js
+++ routes/MediaRoutes.js
@@ -1,52 +1,65 @@
-const express = require('express'),
+const express = require("express"),
    media = express(),
-   cors = require('cors'),
-   fs = require('fs-extra'),
-   fileUpload = require('express-fileupload'),
-   rf = require('./RoutFuctions');
+   cors = require("cors"),
+   fs = require("fs-extra"),
+   fileUpload = require("express-fileupload"),
+   rf = require("./RoutFuctions");
 
 media.use(cors());
 media.use(fileUpload({ safeFileNames: true, preserveExtension: true }));
 
-media.post('/uploadfile', rf.verifyToken, function(req, res) {
+media.post("/uploadfile", rf.verifyToken, function (req, res) {
+   // establish if it is in developement (on port :3000) or in productions - post to appropriate upload path
+   let refer = req.headers.referer;
+   //using full path (not relative) because path changes depending on server being run as crontab or from prompt
+   let pathToUploadFolder =
+      "/var/www/sites/donation.nelles.io/client/build/upload/";
+   if (refer !== undefined && refer.toString().includes(":3000"))
+      pathToUploadFolder = "../client/public/upload/";
+
    var mime = req.files.files.mimetype.toString();
+   console.log(req.files.files);
 
    //if(fn.includes(".php") || fn.includes(".js") ||  fn.includes(".pl") || fn.includes(".htm")|| fn.includes(".exe") || fn.includes(".txt") || !fn.includes(".")){
    if (
-      mime.includes('image') ||
-      mime.includes('audio') ||
-      mime.includes('video')
+      mime.includes("image") ||
+      mime.includes("audio") ||
+      mime.includes("video")
    ) {
-      req.files.files.mv(
-         '../client/public/upload/' + req.files.files.name,
-         function(err) {
-            if (err) {
-               console.log('Error: ' + err);
-               res.send('Upload failed' + err).end();
-            } else {
-               console.log('Uploaded ok');
-               //res.write("<img src=\"/uploads/up/"+fn+"\" class=\"ui image\" width=\"290\" height=\"160\">");
-               res.end('File uploaded successfully');
-            }
+      req.files.files.mv(pathToUploadFolder + req.files.files.name, function (
+         err
+      ) {
+         if (err) {
+            console.log("Error: " + err);
+            res.send("Upload failed" + err).end();
+         } else {
+            console.log("Uploaded ok");
+            //res.write("<img src=\"/uploads/up/"+fn+"\" class=\"ui image\" width=\"290\" height=\"160\">");
+            res.end("File uploaded successfully");
          }
-      );
+      });
    } else {
-      console.log('Illegal file type');
-      res.send('Illegal file type').end();
+      console.log("Illegal file type");
+      res.send("Illegal file type").end();
    }
 });
 
-media.post('/getmedia', rf.verifyToken, (req, res) => {
-   const path = '../client/public/upload/';
+media.post("/getmedia", rf.verifyToken, (req, res) => {
+   let refer = req.headers.referer;
+   //using full path (not relative) because path changes depending on server being run as crontab or from prompt
+   let pathToUploadFolder =
+      "/var/www/sites/donation.nelles.io/client/build/upload/";
+   if (refer !== undefined && refer.toString().includes(":3000"))
+      pathToUploadFolder = "../client/public/upload/";
 
    var arr = []; // this will be an array of objects
 
-   fs.readdirSync(path, { withFileTypes: true })
+   fs.readdirSync(pathToUploadFolder, { withFileTypes: true })
       .filter((item) => !item.isDirectory())
-      .map(function(item, index) {
+      .map(function (item, index) {
          o = {
             name: item.name,
-            bgc: '-'
+            bgc: "-",
          };
          arr.push(o);
       });
@@ -54,21 +67,25 @@ media.post('/getmedia', rf.verifyToken, (req, res) => {
    res.send(arr);
 });
 
-media.post('/removeFile', rf.verifyToken, (req, res) => {
-   console.log('in removeFile');
-   const path = '../client/public/upload/';
+media.post("/removeFile", rf.verifyToken, (req, res) => {
+   let refer = req.headers.referer;
+   //using full path (not relative) because path changes depending on server being run as crontab or from prompt
+   let pathToUploadFolder =
+      "/var/www/sites/donation.nelles.io/client/build/upload/";
+   if (refer !== undefined && refer.toString().includes(":3000"))
+      pathToUploadFolder = "../client/public/upload/";
    const fileName = req.body.fileName;
 
    // With Promises:
-   fs.remove('/tmp/myfile');
-   fs.remove(path + fileName)
+   fs.remove("/tmp/myfile");
+   fs.remove(pathToUploadFolder + fileName)
       .then(() => {
-         console.log('success delete file: ' + fileName);
-         res.send('ok').end();
+         console.log("success delete file: " + fileName);
+         res.send("ok").end();
       })
       .catch((err) => {
-         console.error('Failed in deleting file ' + fileName + ' Err: ' + err);
-         res.send('failed to delete file').end();
+         console.error("Failed in deleting file " + fileName + " Err: " + err);
+         res.send("failed to delete file").end();
       });
 });
 
